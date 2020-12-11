795dde47ab9fb5ecbd021ec7a8ed970bc0315fdf
jspickerman <jonathanspickerman@gmail.com>
2020-12-11 13:22:32 -0600

    2 files modified

--- src/app/coffee-addition/foam-addition/foam-addition.component.ts
+++ src/app/coffee-addition/foam-addition/foam-addition.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { CoffeeProducts, DairyFoam } from '../../models/coffee-addition';
+import { AdditionTypes, CoffeeProducts, DairyFoam } from '../../models/coffee-addition';
 import { CoffeeOrderService } from '../../services/coffee-order.service';
 import { CoffeeAdditionComponent } from '../coffee-addition.component';
 
@@ -18,12 +18,16 @@ export class FoamAdditionComponent extends CoffeeAdditionComponent implements On
 
   ngOnInit() {
     super.ngOnInit();
+    this.showCappuccinoMessage = this.suggestCappuccino();
   }
 
   public saveSelection(): void {
-    if (this.selectedOptions.length > 0) {
+    console.log('selectd options' , this.selectedOptions);
+    console.log('selected opition ',this.selectedOption);
+    if (this.selectedOptions) {
       this.orderService.addAddition(this.coffeeOrder, {...this.addition, selectedOptions: this.selectedOptions});
-      this.showCappuccinoMessage = this.suggestCappuccino();
+      // this.showCappuccinoMessage = this.suggestCappuccino();
+      // console.log(this.showCappuccinoMessage);
     } else {
       this.orderService.removeAddition(this.coffeeOrder, this.addition);
       this.showCappuccinoMessage = false;
@@ -31,7 +35,10 @@ export class FoamAdditionComponent extends CoffeeAdditionComponent implements On
   }
   
   public suggestCappuccino(): boolean {
-    const extraFoamOption = this.selectedOptions.find(option => option.name === DairyFoam.EXTRA);
+    const foamAddition = this.coffeeOrder.additions.find(addition => addition.name === AdditionTypes.FOAM);
+    console.log(foamAddition);
+    const extraFoamOption = foamAddition && foamAddition.selectedOptions.find(option => option.name === DairyFoam.EXTRA);
+    console.log(extraFoamOption);
     return (
         extraFoamOption && 
         this.coffeeOrder.product.name !== CoffeeProducts.CAPPUCCINO
